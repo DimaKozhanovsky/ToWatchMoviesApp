35d4c4f246d7230e78026adee415410a742de0c9
DimaKozhanovsky <koganovski1989koganovski@gmail.com>
2023-10-04 14:00:57 +0300

    Initial Commit

---
+++ ToWatchMovie/ToWatchMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToWatchMovieApp.swift
+//  ToWatchMovie
+//
+//  Created by Dima Kozhanovsky on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToWatchMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
